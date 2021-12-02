f60e436d60557dc98f63114b0be4c54eb06ea3c4
Anders Petruschke <anders.petruschke@t-online.de>
2021-12-02 21:22:48 +0100

    Initial Commit

---
+++ Shared/Combine_PresentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_PresentationApp.swift
+//  Shared
+//
+//  Created by Anders Petruschke on 02.12.21.
+//
+
+import SwiftUI
+
+@main
+struct Combine_PresentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
